db72361029834eccb700e9f62185f3baf4f5958f
jeff indall <jeffindall@gmail.com>
2018-06-05 15:44:31 -0600

    passing commit

--- app.js
+++ app.js
@@ -1,49 +1,43 @@
-const express = require('express');
-var morgan = require('morgan');
+const express = require("express");
+var morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
-const instructors = require("./instructors.json")
-app.use(morgan('tiny'));
+const instructors = require("./instructors.json");
+app.use(morgan("tiny"));
 
 // fetch("./instructors.json")
 //  .then(response => response.json())
 //  .then(json => console.log(json));
 
-
-
-function findById(data, id){
-    for (let i = 0; i < data.length; i++){
-        if (data[i].id == id){
-            return data[i];
-        }
+function findById(data, id) {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id == id) {
+      return data[i];
     }
-    return null;
+  }
+  return null;
 }
-
 
 app.use(cors());
 
+app.get("/", function(request, response) {
+  response.json({ data: instructors });
+});
+
+app.get("/:id", function(request, response) {
+  var record = findById(instructors, request.params.id);
+  if (!record) {
+    response.status(404).json({
+      error: {
+        message: "No record found!"
+      }
+    });
+  } else {
+    response.json({ data: record });
+  }
+});
 
 app.listen(port, () => {
-    app.get("/", function (request, response) {
-        response.json({data: instructors});
-    });
-    
-    app.get("/:id", function (request, response) {
-        var record = findById(instructors, request.params.id);
-        if (!record){
-            response.status = (404).json({
-                error: {
-                    message: "No record found!"
-                }
-            });
-        } else {
-    
-    response.json({data: record});
-
-    }
-  
-     
-})
-})
+  console.log("server running");
+});
